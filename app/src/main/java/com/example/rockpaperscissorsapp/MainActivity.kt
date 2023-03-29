3d864f1c444bf6cedc538564bf42e1e86974e80a
Ricardo Taipe <rdavid28@hotmail.es>
2023-03-29 18:06:18 -0500

    update ui

--- app/src/main/java/com/example/rockpaperscissorsapp/MainActivity.kt
+++ app/src/main/java/com/example/rockpaperscissorsapp/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.rockpaperscissorsapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.content.res.AppCompatResources
+import com.example.rockpaperscissorsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding.paperIv.setOnClickListener { }
+        binding.rockIv.setOnClickListener { }
+        binding.scissorsIv.setOnClickListener { }
+
+        setContentView(binding.root)
     }
 }
+
+//binding.paperIv.background =
+//AppCompatResources.getDrawable(applicationContext, R.drawable.bg_scissors)
