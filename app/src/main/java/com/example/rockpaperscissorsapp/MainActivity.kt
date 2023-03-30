9e6a4cc465a8777ce1ca1977a2c58ad64f40e8b3
Ricardo Taipe <rdavid28@hotmail.es>
2023-03-30 14:21:17 -0500

    add navigation

--- app/src/main/java/com/example/rockpaperscissorsapp/MainActivity.kt
+++ app/src/main/java/com/example/rockpaperscissorsapp/MainActivity.kt
@@ -2,20 +2,14 @@ package com.example.rockpaperscissorsapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.content.res.AppCompatResources
+import androidx.navigation.fragment.NavHostFragment
 import com.example.rockpaperscissorsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
-        binding.paperIv.setOnClickListener { }
-        binding.rockIv.setOnClickListener { }
-        binding.scissorsIv.setOnClickListener { }
-
+        supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         setContentView(binding.root)
     }
 }
-
-//binding.paperIv.background =
-//AppCompatResources.getDrawable(applicationContext, R.drawable.bg_scissors)
