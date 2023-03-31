064d25d2318b51d002bce7b0a3b18e7ca60cf10a
Ricardo Taipe <rdavid28@hotmail.es>
2023-03-30 22:39:40 -0500

    add GameTest

--- app/src/main/java/com/example/rockpaperscissorsapp/Game.kt
+++ app/src/main/java/com/example/rockpaperscissorsapp/Game.kt
@@ -14,19 +14,19 @@ class Game {
         LOSE
     }
 
-    private lateinit var compChoice: Choice
+    var compChoice: Choice? = null
     var score = 0
 
     fun randomChoice(): Choice {
         compChoice = Choice.values().random()
-        return compChoice
+        return compChoice as Choice
     }
 
     fun play(userChoice: Choice): Result {
-        return if ((userChoice.ordinal + 1) % 3 == compChoice.ordinal) {
+        return if ((userChoice.ordinal + 1) % 3 == compChoice?.ordinal) {
             if (score > 0) score--
             Result.LOSE
-        } else if (userChoice.ordinal == compChoice.ordinal) {
+        } else if (userChoice.ordinal == compChoice?.ordinal) {
             Result.DRAW
         } else {
             score++
