2fabce056a9701ef0129578b9b4c196b51f17a09
Ricardo Taipe <rdavid28@hotmail.es>
2024-12-07 19:29:13 -0500

    migrate Rules Screen to compose

--- app/src/main/java/com/example/rockpaperscissorsapp/MainActivity.kt
+++ app/src/main/java/com/example/rockpaperscissorsapp/MainActivity.kt
@@ -1,25 +1,33 @@
 package com.example.rockpaperscissorsapp
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.NavHostFragment
 import com.example.rockpaperscissorsapp.databinding.ActivityMainBinding
 import com.example.rockpaperscissorsapp.model.GameViewModel
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     private lateinit var binding: ActivityMainBinding
     private val gameViewModel by viewModels<GameViewModel> { GameViewModel.Factory }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.lifecycleOwner = this
-        binding.viewModel = gameViewModel
-        supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        binding.rulesButton.setOnClickListener {
-            RulesFragment().show(supportFragmentManager, RulesFragment.TAG)
+//        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+//        binding.lifecycleOwner = this
+//        binding.viewModel = gameViewModel
+//        supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+//        binding.rulesButton.setOnClickListener {
+//            RulesFragment().show(supportFragmentManager, RulesFragment.TAG)
+//        }
+        installSplashScreen()
+        setContent {
+            RulesScreen()
         }
     }
 
