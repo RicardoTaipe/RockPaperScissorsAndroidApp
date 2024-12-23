9e55cde69ffc61ddfdd881dd18435adcb87b5d88
Ricardo Taipe <rdavid28@hotmail.es>
2024-12-22 19:41:49 -0500

    refactor code in GameFragment

--- app/src/main/java/com/example/rockpaperscissorsapp/game/GameFragment.kt
+++ app/src/main/java/com/example/rockpaperscissorsapp/game/GameFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.OnBackPressedCallback
-import androidx.annotation.DrawableRes
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.view.isInvisible
 import androidx.core.view.isVisible
@@ -13,10 +12,10 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.example.rockpaperscissorsapp.R
-import com.example.rockpaperscissorsapp.databinding.FragmentGameBinding
 import com.example.rockpaperscissorsapp.data.Choice
-import com.google.android.material.imageview.ShapeableImageView
 import com.example.rockpaperscissorsapp.data.Result
+import com.example.rockpaperscissorsapp.databinding.FragmentGameBinding
+import com.google.android.material.imageview.ShapeableImageView
 
 class GameFragment : Fragment() {
     private lateinit var binding: FragmentGameBinding
@@ -27,90 +26,79 @@ class GameFragment : Fragment() {
             this,
             object : OnBackPressedCallback(true) {
                 override fun handleOnBackPressed() {
-                    gameViewModel.resetGame()
-                    if (isEnabled) {
-                        isEnabled = false
-                        requireActivity().onBackPressedDispatcher.onBackPressed()
-                    }
+                    resetGameAndHandleBackPress()
                 }
             })
+    }
+
+    private fun resetGameAndHandleBackPress() {
+        gameViewModel.resetGame()
+        requireActivity().onBackPressedDispatcher.onBackPressed()
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentGameBinding.inflate(inflater, container, false)
-        binding.apply {
+        return FragmentGameBinding.inflate(inflater, container, false).run {
+            binding = this
             lifecycleOwner = viewLifecycleOwner
             viewModel = gameViewModel
+            root
         }
-        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        gameViewModel.yourChoice.observe(viewLifecycleOwner) {
-            it ?: return@observe
-            setUiData(binding.userChoice, it)
+        gameViewModel.yourChoice.observe(viewLifecycleOwner) { choice ->
+            choice?.let { setUiData(binding.userChoice, it) }
         }
 
-        gameViewModel.comChoice.observe(viewLifecycleOwner) {
-            it ?: return@observe
-            setUiData(binding.comChoice, it)
-            binding.run {
-                comChoice.isVisible = true
-                timer.isInvisible = true
+        gameViewModel.comChoice.observe(viewLifecycleOwner) { choice ->
+            choice?.let {
+                setUiData(binding.comChoice, it)
+                binding.apply {
+                    comChoice.isVisible = true
+                    timer.isInvisible = true
+                }
             }
-
         }
 
         binding.playAgain.setOnClickListener {
-            findNavController().navigate(R.id.action_gameFragment_to_playFragment)
             gameViewModel.resetGame()
+            findNavController().navigate(R.id.action_gameFragment_to_playFragment)
         }
 
-        gameViewModel.result.observe(viewLifecycleOwner) {
-            it ?: return@observe
-            binding.result.apply {
-                text = getString(
-                    when (it) {
-                        Result.WIN -> R.string.you_win
-                        Result.DRAW -> R.string.draw
-                        Result.LOSE -> R.string.you_lose
-                    }
-                )
-                isVisible = true
+        gameViewModel.result.observe(viewLifecycleOwner) { gameResult ->
+            gameResult?.let {
+                with(binding) {
+                    result.text = getString(
+                        when (it) {
+                            Result.WIN -> R.string.you_win
+                            Result.DRAW -> R.string.draw
+                            Result.LOSE -> R.string.you_lose
+                        }
+                    )
+                    result.isVisible = true
+                    binding.playAgain.isVisible = true
+                }
             }
-            binding.playAgain.isVisible = true
         }
     }
 
     private fun setUiData(view: ShapeableImageView, data: Choice) {
-        @DrawableRes
-        val drawable: Int
-        val image: Int
-
-        when (data) {
-            Choice.ROCK -> {
-                drawable = R.drawable.bg_rock
-                image = R.drawable.icon_rock
-            }
-
-            Choice.PAPER -> {
-                drawable = R.drawable.bg_paper
-                image = R.drawable.icon_paper
-            }
-
-            Choice.SCISSORS -> {
-                drawable = R.drawable.bg_scissors
-                image = R.drawable.icon_scissors
-            }
-        }
-
+        val (background, icon) = getUiDataForChoice(data)
         view.apply {
-            setImageResource(image)
-            background = AppCompatResources.getDrawable(requireContext(), drawable)
+            setImageResource(icon)
+            this.background = AppCompatResources.getDrawable(requireContext(), background)
+        }
+    }
+
+    private fun getUiDataForChoice(data: Choice): Pair<Int, Int> {
+        return when (data) {
+            Choice.ROCK -> Pair(R.drawable.bg_rock, R.drawable.icon_rock)
+            Choice.PAPER -> Pair(R.drawable.bg_paper, R.drawable.icon_paper)
+            Choice.SCISSORS -> Pair(R.drawable.bg_scissors, R.drawable.icon_scissors)
         }
     }
 }
