c0355d6391fe51c795f6d09c855af4625ae50bf2
Ricardo Taipe <rdavid28@hotmail.es>
2024-12-25 20:36:55 -0500

    add instrumented test to PlayFragment

--- app/src/androidTest/java/com/example/rockpaperscissorsapp/play/PlayFragmentTest.kt
+++ app/src/androidTest/java/com/example/rockpaperscissorsapp/play/PlayFragmentTest.kt
@@ -4,36 +4,62 @@ import android.os.Bundle
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.rockpaperscissorsapp.R
+import com.example.rockpaperscissorsapp.RockPaperScissorsApplication
+import com.example.rockpaperscissorsapp.data.Choice
+import com.example.rockpaperscissorsapp.data.GameRepository
+import com.example.rockpaperscissorsapp.di.AppContainer
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.Mock
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
+import org.mockito.Mockito.`when`
+import org.mockito.MockitoAnnotations
 
 @RunWith(AndroidJUnit4::class)
 class PlayFragmentTest {
+    @Mock
+    private lateinit var mockContainer: AppContainer
+
+    @Mock
+    private lateinit var gameRepository: GameRepository
+
+    @Before
+    fun registerIdlingResource() {
+        MockitoAnnotations.openMocks(this)
+        getApplicationContext<RockPaperScissorsApplication>().container = mockContainer
+        `when`(mockContainer.gameRepository).thenReturn(gameRepository)
+    }
+
     @Test
     fun clickRockButton_navigateToGameFragment() {
         testButtonNavigation(R.id.rock_iv)
+        verify(gameRepository).userChoice = Choice.ROCK
     }
 
     @Test
     fun clickPaperButton_navigateToGameFragment() {
         testButtonNavigation(R.id.paper_iv)
+        verify(gameRepository).userChoice = Choice.PAPER
     }
 
     @Test
     fun clickScissorButton_navigateToGameFragment() {
         testButtonNavigation(R.id.scissors_iv)
+        verify(gameRepository).userChoice = Choice.SCISSORS
     }
 
     // Helper function to test button click and navigation
     private fun testButtonNavigation(buttonId: Int) {
-        val scenario = launchFragmentInContainer<PlayFragment>(Bundle(), R.style.Theme_RockPaperScissorsApp)
+        val scenario =
+            launchFragmentInContainer<PlayFragment>(Bundle(), R.style.Theme_RockPaperScissorsApp)
         val navController = mock(NavController::class.java)
 
         scenario.onFragment {
--- app/src/main/java/com/example/rockpaperscissorsapp/data/GameRepository.kt
+++ app/src/main/java/com/example/rockpaperscissorsapp/data/GameRepository.kt
@@ -3,5 +3,6 @@ package com.example.rockpaperscissorsapp.data
 interface GameRepository {
     val score: Int
     fun getRandomComputerChoice(): Choice
-    fun play(userChoice: Choice): Result
+    fun play(): Result
+    var userChoice: Choice
 }
